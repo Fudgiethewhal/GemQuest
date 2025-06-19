dbf1ff4d15e59ba7903e978b89eb0932af029df4
Bryseida Hernandez <bryseida.hernandez@yahoo.com>
2025-06-18 23:01:24 -0700

    added update and clear button

---
+++ Ecommercegq/Ecommercegq/Admin/Category.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ecommercegq.Admin
+{
+    public partial class Category : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+#pragma warning disable IDE1006 // Naming Styles
+        protected void btnAddOrUpdate_Click(object sender, EventArgs e)
+#pragma warning restore IDE1006 // Naming Styles
+        {
+
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
