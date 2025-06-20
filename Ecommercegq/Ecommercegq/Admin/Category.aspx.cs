ec759c65a57a6cc297d5283302cdeba9b86e2e5c
Bryseida Hernandez <bryseida.hernandez@yahoo.com>
2025-06-19 20:19:47 -0700

    adding connection strings

--- Ecommercegq/Ecommercegq/Admin/Category.aspx.cs
+++ Ecommercegq/Ecommercegq/Admin/Category.aspx.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,15 +12,23 @@ namespace Ecommercegq.Admin
 {
     public partial class Category : System.Web.UI.Page
     {
+        MySqlConnection con;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            MySqlConnection con;
+            MySqlCommand cmd;
+            MySqlDataAdapter sda;
+            DataTable dt;
         }
 
 #pragma warning disable IDE1006 // Naming Styles
         protected void btnAddOrUpdate_Click(object sender, EventArgs e)
 #pragma warning restore IDE1006 // Naming Styles
         {
+            string actionName= string.Empty, imagePath = string.Empty, fileExtension = string.Empty;
+            bool isValidToExecute = false;
+            int categoryId = Convert.ToInt32(hfCategoryId.Value);
+            MySqlConnection con = new MySqlConnection("server=localhost;user id=root;password=yourpassword;database=ECommerce");
 
         }
 
---
+++ Ecommercegq/Ecommercegq/Utils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Ecommercegq
+{
+    public class Utils
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter sda;
+        SqlDataReader sdr;
+        DataTable dt;
+
+        public static string getConnection()
+        {
+            return ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+        }
+    }
+}
