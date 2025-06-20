b9ad456ca200fc5d2fb8b81c0e22c821c88900dc
Bryseida Hernandez <bryseida.hernandez@yahoo.com>
2025-06-19 22:21:38 -0700

    Connection strings successful

--- Ecommercegq/Ecommercegq/Admin/Category.aspx.cs
+++ Ecommercegq/Ecommercegq/Admin/Category.aspx.cs
@@ -3,8 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Services.Protocols;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -12,13 +14,13 @@ namespace Ecommercegq.Admin
 {
     public partial class Category : System.Web.UI.Page
     {
-        MySqlConnection con;
+           MySqlConnection con;
+           MySqlCommand cmd;
+           MySqlDataAdapter sda;
+           DataTable dt;
         protected void Page_Load(object sender, EventArgs e)
         {
-            MySqlConnection con;
-            MySqlCommand cmd;
-            MySqlDataAdapter sda;
-            DataTable dt;
+            lblMsg.Visible = false;
         }
 
 #pragma warning disable IDE1006 // Naming Styles
@@ -28,13 +30,81 @@ namespace Ecommercegq.Admin
             string actionName= string.Empty, imagePath = string.Empty, fileExtension = string.Empty;
             bool isValidToExecute = false;
             int categoryId = Convert.ToInt32(hfCategoryId.Value);
-            MySqlConnection con = new MySqlConnection("server=localhost;user id=root;password=yourpassword;database=ECommerce");
+            MySqlConnection con = new MySqlConnection(Utils.getConnection());
+            MySqlCommand cmd = new MySqlCommand("Category_Crud", con);
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                // Add parameters: use ?paramName for MySQL
+                cmd.Parameters.AddWithValue("?in_Action", categoryId == 0 ? "INSERT" : "UPDATE");
+                cmd.Parameters.AddWithValue("?in_CategoryId", categoryId);
+                cmd.Parameters.AddWithValue("?in_CategoryName", txtCategoryName.Text.Trim());                
+                cmd.Parameters.AddWithValue("?in_IsActive", cbIsActive.Checked);
+                if (fuCategoryImage.HasFile)
+                {
+                    if(Utils.isValidExtension(fuCategoryImage.FileName))
+                    {
+                        string newImageName = Utils.getUniqueId();
+                        fileExtension = Path.GetExtension(fuCategoryImage.FileName);
+                        imagePath = "Images/Category/" + newImageName.ToString() + fileExtension;
+                        fuCategoryImage.PostedFile.SaveAs(Server.MapPath("~/Images/Category/") + newImageName.ToString() + fileExtension);
+                        cmd.Parameters.AddWithValue("?in_CategoryImageUrl", imagePath);
+                        isValidToExecute = true;
+                    }
+                    else
+                    {
+                        lblMsg.Visible = false;
+                        lblMsg.Text = "Please upload a valid image file (jpg, jpeg, png).";
+                        lblMsg.CssClass = "alert alert-danger";
+                        isValidToExecute = false;
+                    }
+ 
+                }
+                else
+                {
+                    isValidToExecute = true;
+                }
+                if (isValidToExecute)
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    try
+                    {
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                        actionName = categoryId == 0 ? " inserted " : " updated ";
+                        lblMsg.Visible = true;
+                        lblMsg.Text = " Category " + actionName + " successfully!";
+                        lblMsg.CssClass = "alert alert-success";
+                    }
+                    catch (Exception ex)
+                    {
+                        lblMsg.Visible = true;
+                        lblMsg.Text = "Error- " + ex.Message;
+                        lblMsg.CssClass = "alert alert-danger";
+                    }
+                    finally
+                    {
+                        con.Close();
+                    }
+
+                }
+                
+            }
 
         }
-
         protected void btnClear_Click(object sender, EventArgs e)
         {
-
+            clear();
         }
+
+        public void clear()
+        {
+            txtCategoryName.Text = string.Empty;
+            cbIsActive.Checked = false;
+            hfCategoryId.Value = "0";
+            btnAddOrUpdate.Text = "Add";
+            imagePreview.ImageUrl = string.Empty;
+        }
+
     }
 }
--- Ecommercegq/Ecommercegq/Utils.cs
+++ Ecommercegq/Ecommercegq/Utils.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
-using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
 using System.Linq;
 using System.Web;
 
@@ -10,15 +10,36 @@ namespace Ecommercegq
 {
     public class Utils
     {
-        SqlConnection con;
-        SqlCommand cmd;
-        SqlDataAdapter sda;
-        SqlDataReader sdr;
+        MySqlConnection con;
+        MySqlCommand cmd;
+        MySqlDataAdapter sda;
+        MySqlDataReader sdr;
         DataTable dt;
 
         public static string getConnection()
         {
             return ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+        }
+
+        public static bool isValidExtension(string fileName)
+        {
+            bool isValid = false;
+            string[] fileExtension = { ".jpg", ".jpeg", ".png" };
+            foreach (string file in fileExtension)
+            {
+                if (fileName.Contains(file))
+                {
+                    isValid = true;
+                    break;
+                }
+            }
+            return isValid;
+        }
+
+        public static string getUniqueId()
+        {
+            Guid guid = Guid.NewGuid();
+            return guid.ToString();
         }
     }
 }
